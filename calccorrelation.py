71e72b09d37db94bb7286c1ab87908fd5d048b86
Vu Ngo <vungo2004@gmail.com>
2016-05-22 03:04:39 -0700

    update

--- calccorrelation.py
+++ calccorrelation.py
@@ -1,22 +1,23 @@
 """
 Input:  files:
 		 1 is the input-normalized_coverage mark bed file (binned)
-		 1 is the peak regions of the histone mark (binned)
+		 all the peak regions of each tissue-stage (binned)
 		 1 is the score for each motif. 
 Output:
 Correlation between the motif and the histone mark 
 """
 import time
 
-
-
+import os
 from sys import argv
 from scipy import stats
 def main():
-	coveragefile=("./WorkingDCCdata/DCC_regions_bin500/test/Cranio-Facial_E11.5.test.bed")
+	coveragedir=("./WorkingDCCdata/DCC_regions_bin500/test/Cranio-Facial_E11.5.test.bed")
+	coveragefiles=os.listdir(coveragedir)
+
 	scorefile=("./WorkingDCCdata/Regions_forscanning/Scanning_results/0_motif.100_Forebrain_E13.5_H3K4me162_0.503_7.785084e-24_12.motif.scanned")
-	peakfile=("./WorkingDCCdata/DCC_regions_bin500/Cranio-Facial_E11.5_H3K27ac.DCC.narrowPeaks.mark.bin500.bed")
-	print "read Peaks"
+	#peakfile=("./WorkingDCCdata/DCC_regions_bin500/Cranio-Facial_E11.5_H3K27ac.DCC.narrowPeaks.mark.bin500.bed")
+	'''print "read Peaks"
 	peaks={}
 	for line in open(peakfile):
 		tmp=line.strip().split('\t')
@@ -27,45 +28,37 @@ def main():
 		except KeyError:
 			peaks[chrom]={}
 			peaks[chrom][start]=1
-	
-	start = time.time()
-	print("hello")
+	'''
 	
 	print "read scores"
-	scores_cov={}
+	scores={}
 	for line in open(scorefile):
 		tmp=line.strip().split('\t')
 		s=float(tmp[1])
 		name=tmp[0].split('_')
 		chrom=name[0]
 		loc=name[1]
-		if chrom in peaks and loc in peaks[chrom]:
-			scores_cov['_'.join(name)]=[s,0]
-	
-	end = time.time()
-	print(end - start)
-	start = time.time()
-	print("hello")
+		scores[chrom+'_'+loc]=s
+
 	print "read covs"
-	for line in open(coveragefile):
-		tmp=line.strip().split('\t')
-		cov=float(tmp[1])
-		chrom=tmp[0]
-		loc=tmp[1]
-		name=chrom+'_'+loc
-		if  name in scores_cov:
-			scores_cov[name][1]=cov
-	end = time.time()
-	print(end - start)
-	print "get vectors"
-	scorevector=[]
-	covector=[]
-	for name in scores_cov:
-		scorevector+=[scores_cov[name][0]]
-		covector+=[scores_cov[name][1]]
-	print "calc corl"
-	correlation=stats.spearmanr(scorevector, covector)
-	print correlation
+	for file in coveragefiles:
+		print file
+		coverages={}
+		for line in open(file):
+			tmp=line.strip().split('\t')
+			cov=float(tmp[1])
+			name=tmp[0]
+			coverages[name]=cov
+
+		print "get vectors"
+		scorevector=[]
+		covector=[]
+		for name in coverages:
+			scorevector+=[scores[name][0]]
+			covector+=[coverages[name][1]]
+		print "calc corl"
+		correlation=stats.spearmanr(scorevector, covector)
+		print correlation
 
 	return
 
---
+++ filterbedentry_macro.py
@@ -0,0 +1,22 @@
+import os
+from sys import agrv
+
+def main():
+	covfile=argv[1]
+	basecovfile=covfile.split("/")[-1]
+	tisuestage=basecovfile.split('_')[:2]
+	mark=basecovfile.split('_')[2]
+	outfile='_'.join(tisuestage+[mark])+".bin500.filtered.coverage"
+	if "Cran" in tisuestage[0]:
+		tisuestage[0]="Cranio"
+	if "Neural" in tisuestage[0]:
+		tisuestage[0]="Neural"
+
+	regionfile='_'.join(tisuestage)+".sorted.merged.bin500"
+	pathtoregion='/home/vungo/MouseENCODE_project/WorkingDCCdata/DCC_regions_bin500/X.mergedregions/'
+	cmd="python ~/MouseENCODE_project/mENCODE_scripts/filterbedentry.py "+covfile+" "+pathtoregion+regionfile+" "+outfile
+	print cmd
+	return
+
+if __name__=="__main__":
+	main()
