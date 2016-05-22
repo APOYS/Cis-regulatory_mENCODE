f01cd928131afde2219b06df06edc32a14f91de6
Vu Ngo <vungo2004@gmail.com>
2016-05-21 21:56:30 -0700

    update

---
+++ calccorrelation.py
@@ -0,0 +1,15 @@
+"""
+Input:  files:
+		 1 is the input-normalized_coverage mark bed file (binned)
+		 1 is the peak regions of the histone mark (binned)
+		 1 is the score for each motif. 
+Output:
+Correlation between the motif and the histone mark 
+"""
+
+def main():
+
+	return
+
+if __name__=="__main__":
+	main()
