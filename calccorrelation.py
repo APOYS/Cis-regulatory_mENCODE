ff99f85a46fa8425fcabe8f24ebd04bb17e5a0ea
Vu Ngo <vungo2004@gmail.com>
2016-05-22 01:56:19 -0700

    update

--- calccorrelation.py
+++ calccorrelation.py
@@ -6,9 +6,54 @@ Input:  files:
 Output:
 Correlation between the motif and the histone mark 
 """
-
+from sys import argv
+from scipy import stats
 def main():
+	coveragefile=argv[1]
+	scorefile=argv[2]
+	peakfile=argv[3]
 	
+	peaks={}
+	for line in open(peakfile).read().strip().split('\n'):
+		tmp=line.strip().split('\t')
+		chrom=tmp[0]
+		start=tmp[1]
+		try:
+			peaks[chrom][start]=1
+		except KeyError:
+			peaks[chrom]={}
+			peaks[chrom][start]=1
+
+	scores_cov={}
+	for line in open(scorefile).read().strip().split('\n'):
+		tmp=line.strip().split('\t')
+		s=float(tmp[1])
+		name=tmp[0].split('_')
+		chrom=name[0]
+		loc=name[1]
+		if chrom in peaks and loc in peaks[chrom]:
+			try:
+				scores_cov[chrom][loc]=[s,0]
+			except KeyError:
+				scores_cov[chrom]={}
+				scores_cov[chrom][loc]=[s,0]
+	for line in open(coveragefile).read().strip().split('\n'):
+		tmp=line.strip().split('\t')
+		cov=float(tmp[3])
+		chrom=tmp[0]
+		loc=tmp[1]
+		if chrom in scores_cov and loc in scores_cov[chrom]:
+			scores_cov[chrom][loc][1]=cov
+	
+	scorevector=[]
+	covector=[]
+	for chrom in scores_cov:
+		for loc in scores_cov[chrom]:
+			scorevector+=[scores_cov[chrom][loc][0]]
+			covector+=[scores_cov[chrom][loc][1]]
+	correlation=stats.spearmanr(scorevector, covector)
+	print correlation
+
 	return
 
 if __name__=="__main__":
---
+++ filterbedentry.py
@@ -0,0 +1,32 @@
+"""
+Input: 2 files
+	- coverage bedfile
+	- merged peak file
+
+Out: filtered coverage bedfile
+"""
+from sys import argv
+
+def main():
+	covfile=argv[1]
+	peakfile=argv[2]
+	outfile=argv[3]
+
+	peaks={}
+	for line in open(peakfile):
+		tmp=line.split('\t')
+		name=tmp[0]+'_'+tmp[1]
+		peaks[name]=1
+
+	out=open(outfile,'w')
+	for line in open(covfile):
+		tmp=line.split('\t')
+		name=tmp[0]+"_"+tm[1]
+		if name in peaks:
+			out.write(name+"\t"+tmp[3])
+	out.close()
+
+	return
+
+if __name__=="__main__":
+	main()
