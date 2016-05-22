7933a9baca61f1679f06dad2e6922b52482a5c67
Vu Ngo <vungo2004@gmail.com>
2016-05-21 22:19:26 -0700

    update

--- coverageInputNormalize.py
+++ coverageInputNormalize.py
@@ -8,11 +8,12 @@ from sys import argv
 
 def main():
 	inputbed=argv[1]
+	#inputbed="Midbrain_E12.5_Input_Rep0_ENCFF595MDP_ENCFF233LRZ.bed.500bin.coverage"
 
 	prefix=inputbed.strip().split("Input")[0]+"H3K"
 	print "Working on",prefix,'...'
-	command="""for file in Cranio-facial_E11.5_H3K*.coverage; 
-	do echo $file;input=Cranio-facial_E11.5_Input_Rep0_ENCFF851KTA_ENCFF265KAG.bed.500bin.coverage; 
+	command="""for file in """+prefix+"""*.coverage; 
+	do echo $file;input="""+inputbed+"""; 
 	paste $file $input |awk 'function abs(value){return (value<0?0:value);};{ printf("%s\t%d\t%d\t%s\n", $1, $2, $3,abs($4-$11)); }' > $file.normalized;
 	done"""
 	print command
