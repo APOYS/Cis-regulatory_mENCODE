1922b9ae2cb0d7b6216d7fffdd014777a9b70c2a
Vu Ngo <vungo2004@gmail.com>
2016-05-21 22:16:10 -0700

    update

--- coverageInputNormalize.py
+++ coverageInputNormalize.py
@@ -16,6 +16,8 @@ def main():
 	paste $file $input |awk 'function abs(value){return (value<0?0:value);};{ printf("%s\t%d\t%d\t%s\n", $1, $2, $3,abs($4-$11)); }' > $file.normalized;
 	done"""
 	print command
+	os.system(command)
+
 	return
 
 
