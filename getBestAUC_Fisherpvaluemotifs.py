d2f0b95e1660270d4c059b0ee07e7937430de1b1
Vu Ngo <vungo2004@gmail.com>
2016-05-31 21:46:06 -0700

    update

---
+++ getBestAUC_Fisherpvaluemotifs.py
@@ -0,0 +1,5 @@
+"""
+IN: the mark.full.meme file, AUC cutoff, fisher pvalue cutoff
+OUT: the mark.full.meme with the motifs with best AUC and fisher pvalue
+
+"""
--- getBestmotifs_from_epigram.py
+++ getBestmotifs_from_epigram.py
@@ -1,5 +1,5 @@
 """
-INPUT: fullmodel meme file. fisher p-value file (from epigram), p-value cutoff, enrichement cutoff
+INPUT: mark-epigram.meme file. fisher p-value file (from epigram), auc file, p-value cutoff, enrichement cutoff
 
 OUTPUT: best motifs according to the cutoffs in a .meme file
 """
@@ -7,12 +7,20 @@ OUTPUT: best motifs according to the cutoffs in a .meme file
 from sys import argv
 
 def main():
-	#memefile=argv[1]
-	#fisherfile=argv[2]
-	#pvaluecutoff=float(argv[3])
-	#enrichmentcutoff=float(argv[4])
-	fisherfile="/Users/vungo/Downloads/fisher.pvalues.tsv"
+	memefile=argv[1]
+	fisherfile=argv[2]
+	aucfile=argv[3]
+	enrichmentcutoff=float(argv[4])
+	outfile=argv[5]
+	pvaluecutoff=1e-20
 
+
+	#outfile="/Users/vungo/Downloads/epigramtest.bestmotifs.txt"
+	#aucfile="/Users/vungo/Downloads/epigramtest.auc.txt"
+	#fisherfile="/Users/vungo/Downloads/epigramtest.fisher.txt"
+	
+	#enrichmentcutoff=1.5
+	#print pvaluecutoff
 	#read the fisher file:
 	fishertable=[]
 	for line in open(fisherfile):
@@ -20,14 +28,30 @@ def main():
 		for i in range(3,len(tmp)):
 			tmp[i]=float(tmp[i])
 			#print tmp, tmp[6],tmp[8],i[6]/i[8]
-		fishertable+=[tmp[1:2]+[tmp[3]]+[tmp[6]]+[tmp[8]]+[tmp[6]/tmp[8]]]
+		if tmp[2]=="P":
+			fishertable+=[tmp[1:2]+[tmp[3]]+[tmp[6]]+[tmp[8]]+[tmp[6]/tmp[8]]]
+	
+	motifstokeep=[]
 	for i in fishertable:
-		if i[-1]>1.5:
-			print i
-		
-	#add the enrichement to the table:
-
-
+		if i[-1]>enrichmentcutoff and i[1]<pvaluecutoff:
+			#print i
+			motifstokeep+=[i]
+	aucscores={}
+	for line in open(aucfile):
+		tmp=line.strip().split('\t')
+		aucscores[tmp[1]]=float(tmp[2])
+	#print aucscores
+	out=open(outfile,'w')
+	for line in motifstokeep:
+		if aucscores[line[0]]>0.5:
+			#print line[0],line[1],line[-1],aucscores[line[0]]
+			if line[1]==0:
+				towrite=line[0].split('_')[0]+'_'+str('%.3f' %aucscores[line[0]])
+				out.write(towrite+'\n')
+			else:
+				towrite=line[0].split('_')[0]+'_'+str('%.3f' %aucscores[line[0]])
+				out.write(towrite+'\n')
+	out.close()
 
 	return
 
