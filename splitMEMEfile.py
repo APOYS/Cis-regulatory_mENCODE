488c0c2e8c9a328656de8eb9636dfd8fedadad1a
Vu Ngo <vungo2004@gmail.com>
2016-05-31 23:06:05 -0700

    update

---
+++ splitMEMEfile.py
@@ -0,0 +1,34 @@
+"""
+IN: MEMEfile, number of subfile k
+OUT: k subfiles 
+"""
+
+from sys import argv
+
+def main():
+	memefile=argv[1]
+	k=int(argv[2])
+
+	#k=15
+	binsize=k
+
+	motifs=open(memefile).read().strip().split('MOTIF')
+	motifs=motifs[1:]
+
+	count=1
+	out=open(memefile+"."+str(count)+".meme",'w')
+	for m in range(len(motifs)):
+		if m!=0 and m%binsize==0:
+			out.close()
+			count+=1
+			out=open(memefile+"."+str(count)+".meme",'w')
+		else:
+			out.write(str(m)+'\n')
+	out.close()
+	
+	return
+
+if __name__=="__main__":
+	main()
+
+
