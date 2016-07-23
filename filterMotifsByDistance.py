7ab4a75bb2cb5d9d29f6b452c50ce6756226b2bb
Vu Ngo <vungo2004@gmail.com>
2016-07-22 17:02:01 -0700

    add data

---
+++ filterMotifsByDistance.py
@@ -0,0 +1,19 @@
+"""
+IN: distance file for all pair-wise distances between, distance cutoff D
+
+OUT: list of motifs 
+
+"""
+
+from sys import argv
+
+def main():
+	infile=argv[1]
+	Dcutoff=float(argv[2])
+
+	
+	return 
+
+
+if __name__=="__main__":
+	main()
