42aa12d890dfa0dc3e72c73822b142c199e4fc9f
Vu Ngo <vungo2004@gmail.com>
2016-08-10 02:25:02 -0700

    adding filter by similarity distance

--- convertMEMEFormatForScanning.py
+++ convertMEMEFormatForScanning.py
@@ -7,12 +7,13 @@ OUT: several PWM files , the names are in the form of 315_motif.71_Limb_E14.5_H3
 
 from sys import argv
 
-def load_motifs(filename):
+def load_motifsSpecial(filename):
     file=open(filename)
     seq=file.read().split("MOTIF")
     seq=seq[1:]
     motifs={}
     infos={}
+    pseudocount=0.001
     for s in range(len(seq)):
         t=seq[s].strip().split("\n")
         motifs[t[0]]=t[2:]
@@ -21,10 +22,10 @@ def load_motifs(filename):
         tdict={'A':[],'C':[],'G':[],'T':[]}
         for pos in range(len(motifs[m])):
             tmp=motifs[m][pos].strip().split("\t")
-            tdict['A']+=[float(tmp[0])]
-            tdict['C']+=[float(tmp[1])]
-            tdict['G']+=[float(tmp[2])]
-            tdict['T']+=[float(tmp[3])]
+            tdict['A']+=[(float(tmp[0])+pseudocount)/(1.0+4*pseudocount)]
+            tdict['C']+=[(float(tmp[1])+pseudocount)/(1.0+4*pseudocount)]
+            tdict['G']+=[(float(tmp[2])+pseudocount)/(1.0+4*pseudocount)]
+            tdict['T']+=[(float(tmp[3])+pseudocount)/(1.0+4*pseudocount)]
             #tdict['E']+=[float(tmp[4])]
         motifs[m]=tdict
     
@@ -35,7 +36,7 @@ def main():
 	inmeme=argv[1]
 	outputdir=argv[2]
 	
-	motifs,infos=load_motifs(inmeme)
+	motifs,infos=load_motifsSpecial(inmeme)
 	motifnames=sorted(motifs.keys())
 	klength=0
 	#count=0
