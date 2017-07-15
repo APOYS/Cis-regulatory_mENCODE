f0259c10f28f653ab40602962bf5f1126c8019ce
Vu Ngo <vungo2004@gmail.com>
2017-07-14 21:03:04 -0700

    filter

--- filterGREATresults.py
+++ filterGREATresults.py
@@ -16,7 +16,7 @@ hyper = 0.05
 for line in open("tmp.file"):
 	tmp = line.strip().split('\t')
 	#print tmp
-	if float(tmp[6]) < binorm and float(tmp[15]) < hyper:
+	if float(tmp[6]) < binorm and float(tmp[15]) < hyper and float(tmp[7])>2 and float(tmp[18])>1:
 		L = '\t'.join([tmp[0],tmp[2],tmp[4],tmp[6],tmp[13],tmp[15],tmp[23]])
 		print L
 
