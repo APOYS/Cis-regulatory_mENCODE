b162b51cabf74a29c67f4beb6a4ca5a2e75cf9d8
Vu Ngo <vungo2004@gmail.com>
2017-08-22 14:12:43 -0700

    plot deeptools

--- plot_deeptools_by_chr.py
+++ plot_deeptools_by_chr.py
@@ -105,8 +105,8 @@ def main():
 		
 		for type in types:
 			for mark in marks:
-				cmd = "cat "+outDir+'/'+"%s*%s*5000 > %s/%s/%s.%s.combined.bed" %(type,mark,outDir,chrom,type,mark)
-				#os.system(cmd)
+				cmd = "cat "+outDir+'/'+"%s*%s*%s*5000 > %s/%s/%s.%s.combined.bed" %(type,mark,chrom,outDir,chrom,type,mark)
+				os.system(cmd)
 				print cmd
 
 		# make qsub files for plotting
