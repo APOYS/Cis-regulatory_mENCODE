57b4594896e286268ff004b8f7f7247a747fb9f4
APOYS <vungo2004@gmail.com>
2017-11-09 16:20:09 -0800

    add enrichment calc script for hg19

--- calcMotifEnrichment_hg19.py
+++ calcMotifEnrichment_hg19.py
@@ -16,6 +16,7 @@ parser.add_argument("-b", "--fileB", help="input second bed file")
 parser.add_argument("-g", "--genome", help="genome file in fasta format")
 parser.add_argument("-oa", "--outfileA", help="name of output for file A")
 parser.add_argument("-ob", "--outfileB", help="name of output for file B")
+parser.add_argument("-m","--motiffile", help = "motif file in MEME format")
 
 args = parser.parse_args()
 
@@ -25,6 +26,7 @@ fileB = args.fileB
 genome = args.genome
 outA = args.outfileA
 outB = args.outfileB
+motiffile = args.motiffile
 
 
 #converting the bedfiles into fasta
@@ -41,13 +43,49 @@ os.system(cmd1)
 print cmd2
 os.system(cmd2)
 
-ssbA = faAname+'.ssb'
-ssbB = faBname+'.ssb'
+ssbA = fileA.replace(".bed",'.fa.ssb')
+ssbB = fileB.replace(".bed",'.fa.ssb')
 
 cmd3 = "perl ssb.pl {} {} {} {}".format(faAname,faBname, ssbA, ssbB)
 
 print cmd3
-os.system(cmd1)
+os.system(cmd3)
+
+
+
+# get the Bedfile back from the ssb fa files
+
+ssbBedA = open(ssbA+'.BED','w')
+for line in open(ssbA):
+	if line.startswith(">"):
+		tmp = line.strip().split(":")
+		chrom = tmp[0]
+		s,e = tmp[1].split('-')
+		newline = '\t'.join([chrom,s,e])
+		ssbBedA.write(newline + '\n')
+ssbBedA.close()
+
+ssbBedB = open(ssbB+'.BED','w')
+for line in open(ssbB):
+	if line.startswith(">"):
+		tmp = line.strip().split(":")
+		chrom = tmp[0]
+		s,e = tmp[1].split('-')
+		newline = '\t'.join([chrom,s,e])
+		ssbBedB.write(newline + '\n')
+ssbBedB.close()
+
+
+
+print "Scanning ..."
+print "Foreground"
+
+genomeindex = "/home/vungo/MouseENCODE_project/Kaisscripts/hg19.idx"
+cmd4 = "/home/vungo/MouseENCODE_project/Kaisscripts/motifscan {} {} {} {}".format(genomeindex, motiffile, ssbBedA, outA)
+os.system(cmd4)
+print "Background"
+cmd5 = "/home/vungo/MouseENCODE_project/Kaisscripts/motifscan {} {} {} {}".format(genomeindex, motiffile, ssbBedB, outB)
+os.system(cmd5)
 
 
 
